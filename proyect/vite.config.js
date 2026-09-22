fc31efd711ac49b0713bc7956f9e54cbe69158de
Joserraul <joserraul07@gmail.com>
2026-09-22 15:37:06 -0400

    Update vite.config.js

--- proyect/vite.config.js
+++ proyect/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/MO/', // GitHub Pages sirve el repo en https://joserraul.github.io/MO/
   plugins: [react()],
   define: {
     global: 'globalThis',
